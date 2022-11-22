ceea19f4152ea56b8ae476ff4c06bb0b438459aa
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-22 20:31:09 +0500

    Bukhari.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    pip install requests
    pip install mechanize
    pip install bs4
    rm -rf Bukhari
    git clone --depth=1 https://github.com/MeerHADI-khan/Bukhari.git
    cd Bukhari
    python Bukhari.py

---
+++ Bukhar.py
@@ -0,0 +1,32 @@
+# SAMI
+
+ <b></b> </br> <br>[![Github](https://img.shields.io/badge/github-MeerHADI-Khan-dimgray?style=flat-square&logo=github)](https://github.com/MeerHADI-khan/Bukhari.git)<br> [![Facebook](https://img.shields.io/badge/facebook-SAMI-blue?style=flat-square&logo=facebook)](https://www.facebook.com/MR.SAMI)<br> [![Whatsapp](https://img.shields.io/badge/badge-wthasapp-Bukhari-deepgreen--deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
+
+
+
+<h1 align="center"> [Bukhari]</h1>
+
+<h2 align="center">  FB CLONING TOOL </h2>
+
+
+## <b>installation</b>
+
+ _CLONE FULL OK IDZ_
+
+
+- `pkg update`
+- `pkg upgrade`
+- `pkg install git`
+- `pkg install python`
+- `pip install requests`
+- `pip install mechanize`
+- `pip install bs4`
+- `rm -rf br`
+- `git clone --depth=1 https://github.com/MeerHADI-khan/Bukhari.git`
+- `cd Bukhari`
+- `python Bukhari.py`
+
+
+
+ ___This Tools is Paid___</br>
+ [![whatsapp](https://img.shields.io/badge-wthasapp-Bukhari-deepgreen?style=flat-square&logo=whatsapp)](https://wa.me/+923163372011)
